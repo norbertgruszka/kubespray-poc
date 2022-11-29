4c123f70152f4131158d2274013fd17f580aea2c
Norbert Gruszka <norbert.gruszka@adfinis.com>
2022-11-29 17:12:52 +0100

    Install Cilium as CNI

---
+++ scripts/kubeconfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -uex
+
+ssh -i ./ssh/id_rsa -o StrictHostKeyChecking=no vagrant@192.168.60.32 'sudo chown -R vagrant:vagrant /etc/kubernetes/admin.conf'
+
+scp -i ./ssh/id_rsa vagrant@192.168.60.32:/etc/kubernetes/admin.conf ./admin.conf
+
+sed -i "s/127.0.0.1/192.168.60.32/g" ./admin.conf 
+

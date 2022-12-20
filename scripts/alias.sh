d424facf80767eb1b17da4019d802217886faeae
Norbert Gruszka <norbert.gruszka@adfinis.com>
2022-12-20 14:46:20 +0100

    Commit changes

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,6 +1,3 @@
-alias dcu="docker-compose up | less -R"
-alias dcb="docker-compose build"
-alias dcd="docker-compose down"
-alias play="docker-compose run kubespray /bin/bash -c 'ansible-playbook cluster.yml -i inventory/hosts.yml --become' | tee play_log_$( date +"%Y_%m_%d_%H%M%S" ).txt"
+alias ping=" docker-compose run kubespray /bin/bash -c 'ansible all -m ping -i inventory/hosts.yml -v'"
+alias install="docker-compose run kubespray /bin/bash -c 'ansible-playbook cluster.yml -i inventory/hosts.yml --become' | tee play_log_$( date +"%Y_%m_%d_%H%M%S" ).txt"
 alias upgrade="docker-compose run kubespray /bin/bash -c 'ansible-playbook upgrade-cluster.yml -i inventory/hosts.yml --become' | tee upgrade_log_$( date +"%Y_%m_%d_%H%M%S" ).txt"
-
--- scripts/kubeconfig.sh
+++ scripts/kubeconfig.sh
@@ -2,6 +2,8 @@
 
 set -uex
 
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "192.168.60.32"
+
 ssh -i ./ssh/id_rsa -o StrictHostKeyChecking=no vagrant@192.168.60.32 'sudo chown -R vagrant:vagrant /etc/kubernetes/admin.conf'
 
 scp -i ./ssh/id_rsa vagrant@192.168.60.32:/etc/kubernetes/admin.conf ./admin.conf
